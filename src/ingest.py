8364f6b0ae435637bfa0333c0c5bd54d74aecb37
abirge12 <142052808+abirge12@users.noreply.github.com>
2025-03-20 21:06:58 -0400

    update ingest.py

--- src/ingest.py
+++ src/ingest.py
@@ -10,6 +10,7 @@ import time
 
 REDIS_HOST = "localhost"
 REDIS_PORT = 6380
+REDIS_DB = 0
 
 VECTOR_DIM = 768
 INDEX_NAME = "embedding_index"
@@ -21,10 +22,16 @@ CHUNK_SIZE = 300 # should this be a list? go through different chunk sizes ?
 EMBEDDING_MODEL = "nomic-embed-text" # same Q as above - list of 3 diff embedding models
 
 def get_redis_connection():
-    pass
+    return redis.Redis(
+        host=REDIS_HOST,
+        port=REDIS_PORT,
+        db=REDIS_DB
+    )
 
-def clear_redis_db():
-    pass
+def clear_redis_db(redis_client):
+    print("Clearing Redis database...")
+    redis_client.flushdb()
+    print("Database cleared")
 
 def create_vector_index():
     pass
@@ -35,3 +42,16 @@ def chunk():
 def get_embedding():
     pass
 
+def main():
+
+    redis_client = get_redis_connection()
+
+    # time how long it takes to query
+    start_time = time.time()
+
+
+    elapsed = time.time() - start_time
+
+
+if __name__ == "__main__":
+    main()
